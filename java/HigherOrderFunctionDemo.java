dcf8d82f5179d2a1a6dc925f69dd93d178ba018a
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2021-02-28 16:05:52 -0700

    Adds immutability and higher order functions

--- java/HigherOrderFunctionDemo.java
+++ java/HigherOrderFunctionDemo.java
@@ -1,3 +1,17 @@
+import java.util.function.Function;
+
 public class HigherOrderFunctionDemo {
-	
+	public static void main(String[] args) {
+		final var func = givesFunc();
+		takesFunc(func);
+	}
+
+	private static Function<Integer, Integer> givesFunc() {
+		return a -> 2 * a;
+	}
+
+	private static void takesFunc(Function<Integer, Integer> func) {
+		final var foo = func.apply(42);
+		System.out.println(foo);
+	}
 }
--- java/ImmutabilityDemo.java
+++ java/ImmutabilityDemo.java
@@ -1,3 +1,27 @@
 public class ImmutabilityDemo {
-	
+	public static void main(String[] args) {
+		var bar = "yes";
+		bar = "no";
+		System.out.println(bar);
+
+		final var baz = "howdy";
+		// baz = "later";
+		System.out.println(baz);
+
+		final var thing = new Foo();
+		thing.member = 17;
+		System.out.println(thing.member);
+
+		final var other = new Zap();
+		// other.member = 19;
+		System.out.println(other.member);
+	}
+}
+
+class Foo {
+	public int member = 12;
+}
+
+class Zap {
+	public final int member = 12;
 }
