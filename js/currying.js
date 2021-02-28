fc8d1a7fa807c50035a080c2c169a9a36a04a624
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2021-02-27 17:26:31 -0700

    Starts currying demo

--- js/currying.js
+++ js/currying.js
@@ -1,3 +1,25 @@
 "use strict";
 
+function curryingDemo(name) {
+	const func = foo(name);
+	nextPiece(func);
+}
+
+function nextPiece(func) {
+	const resultOfMeaningfulCalculation = 24;
+	const nextFunc = func(resultOfMeaningfulCalculation);
+	lastPiece(nextFunc);
+}
+
+function lastPiece(func) {
+	const classMember = { string: "howdy" };
+	func(classMember);
+}
+
+const foo = (a) => (b) => (c) => {
+	console.log(`in "foo": a = ${a}`);
+	console.log(`in "foo": b = ${b}`);
+	console.log(`in "foo": c = ${c}`);
+};
+
 console.log("currying.js loaded");
