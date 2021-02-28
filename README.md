5539d54cfb158648bbeaa0cb88af727b5bee04aa
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2021-02-27 17:44:33 -0700

    Starts pure functions demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Usage
+
+## JavaScript
+
+Open [index.html](js/index.html) in your browser, open the developer tools, and call functions from the dev console.
--- js/pureFunctions.js
+++ js/pureFunctions.js
@@ -1,3 +1,29 @@
 "use strict";
 
+// also referential transparency
+
+let state = 1700;
+const resetValue = state;
+
+function pureFunctionDemo(addValue) {
+	const num = impureFunc(addValue);
+	console.log(`num: ${num}`);
+	console.log(`state before: ${resetValue}, state after: ${state}`)
+
+	state = resetValue;
+
+	const num2 = pureFunc(addValue);
+	console.log(`num2: ${num2}`);
+	console.log(`state before: ${resetValue}, state after: ${state}`)
+}
+
+function impureFunc(addValue) {
+	state += addValue;
+	return state;
+}
+
+function pureFunc(addValue) {
+	return state + addValue;
+}
+
 console.log("pureFunctions.js loaded");
