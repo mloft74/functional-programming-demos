b6ce8909fb57af99ed1fc7dc7d2c2fc70be0cd44
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2021-02-27 17:15:07 -0700

    Begins closure demo

--- js/closures.js
+++ js/closures.js
@@ -1,3 +1,20 @@
 "use strict";
 
+function closuresDemo() {
+    const closure = makeClosure(12, 16);
+    closure(5);
+}
+
+function makeClosure(a, b) {
+    const foo = 2 * a;
+    const bar = 3 * b;
+    return function(c) {
+        console.log(`in "makeClosure": a = ${a}`);
+        console.log(`in "makeClosure": b = ${b}`);
+        console.log(`in "makeClosure": foo = ${foo}`);
+        console.log(`in "makeClosure": bar = ${bar}`);
+        console.log(`in "makeClosure": c = ${c}`);
+    }
+}
+
 console.log("closures.js loaded");
