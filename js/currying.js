7aa268c0a4441c6869a400e980a19b80b666d419
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2021-02-27 17:04:02 -0700

    Initial commit

---
+++ js/currying.js
@@ -0,0 +1,3 @@
+"use strict";
+
+console.log("currying.js loaded");
---
+++ js/immutability.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function immutabilityDemo() {
+}
+
+function mutable() {
+
+}
+
+function immutable() {
+
+}
+
+function immutableReference() {
+    const obj = { x: 12 };
+    obj.y = 15;
+    console.log(obj);
+}
+
+console.log("immutability.js loaded");
