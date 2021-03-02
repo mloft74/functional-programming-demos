e9f5c5dc40f0cc1963af4b2577d8ea5adc80266f
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2021-03-01 17:27:13 -0700

    Adds simple example of function passing in Haxe

--- hx/src/Main.hx
+++ hx/src/Main.hx
@@ -1,7 +1,22 @@
 class Main {
 
     public static function main():Void {
-        trace("yo");
+        final foo = giveFunc("Overlord");
+        final result = foo("Nathan", 12);
+        trace(result);
+
+        takeFunc((number, shouldNegate) -> {
+            final output = shouldNegate ? -number : number;
+            trace(output);
+        });
+    }
+
+    public static function giveFunc(title:String):(String, Int) -> String {
+        return (name, age) -> '$title $name: $age years old';
+    }
+
+    public static function takeFunc(func:(Float, Bool) -> Void):Void {
+        func(12.5, true);
     }
 
 }
